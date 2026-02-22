1b0472c5636262f965eae54d3dce7d25bb389fe2
ismailbutthaleem <ib2crt@bolton.ac.uk>
2026-02-22 23:18:24 +0000

    first student pester: this pester is designed to verify the proof of existence of the OU created, if it scans for the OU existence first within the domain and then the desired name of such OU to be correct

---
+++ Tests/Pester/Student-Pester_ProofOfLife.ps1
@@ -0,0 +1,38 @@
+# This Pester test suite verifies the existence of the
+# Organizational Units (OUs) defined in the governance structure.
+#
+# It uses Get-ADOrganizationalUnit to query each OU
+# and asserts that it exists with the expected name.
+#
+# The test fails if any OU is missing or incorrectly named,
+# providing clear feedback about misconfiguration. 
+
+Describe 'Student OU Governance Structure' {
+
+    $DomainDN = 'DC=bolton,DC=corp'
+
+    It 'ControlPlane OU should exist' {
+        (Get-ADOrganizationalUnit -Identity "OU=ControlPlane,$DomainDN" -ErrorAction Stop).Name |
+            Should -Be 'ControlPlane'
+    }
+
+    It 'ManagementPlane OU should exist' {
+        (Get-ADOrganizationalUnit -Identity "OU=ManagementPlane,$DomainDN" -ErrorAction Stop).Name |
+            Should -Be 'ManagementPlane'
+    }
+
+    It 'UserAccessPlane OU should exist' {
+        (Get-ADOrganizationalUnit -Identity "OU=UserAccessPlane,$DomainDN" -ErrorAction Stop).Name |
+            Should -Be 'UserAccessPlane'
+    }
+
+    It 'Users OU should exist under UserAccessPlane' {
+        (Get-ADOrganizationalUnit -Identity "OU=Users,OU=UserAccessPlane,$DomainDN" -ErrorAction Stop).Name |
+            Should -Be 'Users'
+    }
+
+    It 'Computers OU should exist under UserAccessPlane' {
+        (Get-ADOrganizationalUnit -Identity "OU=Computers,OU=UserAccessPlane,$DomainDN" -ErrorAction Stop).Name |
+            Should -Be 'Computers'
+    }
+}
