357fba375c9ec440e795fdff9d97c1c13ea4c693
ismailbutthaleem <ib2crt@bolton.ac.uk>
2026-02-23 03:19:34 +0000

    Add tiered AD users and RBAC membership

--- DSC/Configurations/StudentConfig.ps1
+++ DSC/Configurations/StudentConfig.ps1
@@ -27,7 +27,8 @@ Configuration StudentBaseline {
 
     Node $AllNodes.NodeName {
 
-        $node = $ConfigurationData.AllNodes | Where-Object NodeName -eq $Node.NodeName
+        # FIXED: Where-Object must use a script block
+        $node = $ConfigurationData.AllNodes | Where-Object { $_.NodeName -eq $Node.NodeName }
 
         $Network = @{
             InterfaceAlias = $node.InterfaceAlias_Internal
@@ -154,104 +155,196 @@ Configuration StudentBaseline {
         if ($node.Role -eq 'DC') {
 
             ADOrganizationalUnit 'OU_ControlPlane' {
-                Name  = 'ControlPlane'
-                Path  = $Node.DomainDN
-                Ensure = 'Present'
+                Name                            = 'ControlPlane'
+                Path                            = $Node.DomainDN
+                Ensure                          = 'Present'
                 ProtectedFromAccidentalDeletion = $true
-                DependsOn = '[ADDomain]CreateForest'
+                DependsOn                       = '[ADDomain]CreateForest'
             }
 
             ADOrganizationalUnit 'OU_ManagementPlane' {
-                Name  = 'ManagementPlane'
-                Path  = $Node.DomainDN
-                Ensure = 'Present'
+                Name                            = 'ManagementPlane'
+                Path                            = $Node.DomainDN
+                Ensure                          = 'Present'
                 ProtectedFromAccidentalDeletion = $true
-                DependsOn = '[ADDomain]CreateForest'
+                DependsOn                       = '[ADDomain]CreateForest'
             }
 
             ADOrganizationalUnit 'OU_UserAccessPlane' {
-                Name  = 'UserAccessPlane'
-                Path  = $Node.DomainDN
-                Ensure = 'Present'
+                Name                            = 'UserAccessPlane'
+                Path                            = $Node.DomainDN
+                Ensure                          = 'Present'
                 ProtectedFromAccidentalDeletion = $true
-                DependsOn = '[ADDomain]CreateForest'
+                DependsOn                       = '[ADDomain]CreateForest'
             }
 
             ADOrganizationalUnit 'OU_UserAccessPlane_Users' {
-                Name  = 'Users'
-                Path  = "OU=UserAccessPlane,$($Node.DomainDN)"
-                Ensure = 'Present'
+                Name                            = 'Users'
+                Path                            = "OU=UserAccessPlane,$($Node.DomainDN)"
+                Ensure                          = 'Present'
                 ProtectedFromAccidentalDeletion = $true
-                DependsOn = '[ADOrganizationalUnit]OU_UserAccessPlane'
+                DependsOn                       = '[ADOrganizationalUnit]OU_UserAccessPlane'
             }
 
             ADOrganizationalUnit 'OU_UserAccessPlane_Computers' {
-                Name  = 'Computers'
-                Path  = "OU=UserAccessPlane,$($Node.DomainDN)"
-                Ensure = 'Present'
+                Name                            = 'Computers'
+                Path                            = "OU=UserAccessPlane,$($Node.DomainDN)"
+                Ensure                          = 'Present'
                 ProtectedFromAccidentalDeletion = $true
-                DependsOn = '[ADOrganizationalUnit]OU_UserAccessPlane'
+                DependsOn                       = '[ADOrganizationalUnit]OU_UserAccessPlane'
             }
-            
+
             # OU under UserAccessPlane
-                ADOrganizationalUnit 'OU_UserAccessPlane_Groups' {
+            ADOrganizationalUnit 'OU_UserAccessPlane_Groups' {
                 Name                            = 'Groups'
                 Path                            = 'OU=UserAccessPlane,DC=bolton,DC=corp'
                 ProtectedFromAccidentalDeletion = $true
                 Ensure                          = 'Present'
-            
+                DependsOn                       = '[ADOrganizationalUnit]OU_UserAccessPlane'
             }
-           
+
             # OU under ManagementPlane for admin users
-                ADOrganizationalUnit 'OU_ManagementPlane_AdminUsers' {
+            ADOrganizationalUnit 'OU_ManagementPlane_AdminUsers' {
                 Name                            = 'AdminUsers'
                 Path                            = 'OU=ManagementPlane,DC=bolton,DC=corp'
                 ProtectedFromAccidentalDeletion = $true
                 Ensure                          = 'Present'
+                DependsOn                       = '[ADOrganizationalUnit]OU_ManagementPlane'
             }
+
             # OU under ManagementPlane for groups
-                ADOrganizationalUnit 'OU_ManagementPlane_Groups' {
+            ADOrganizationalUnit 'OU_ManagementPlane_Groups' {
                 Name                            = 'Groups'
                 Path                            = 'OU=ManagementPlane,DC=bolton,DC=corp'
                 ProtectedFromAccidentalDeletion = $true
                 Ensure                          = 'Present'
+                DependsOn                       = '[ADOrganizationalUnit]OU_ManagementPlane'
             }
 
             ADGroup 'GG_HR_Staff' {
-            GroupName  = 'GG-HR-Staff'
-            GroupScope = 'Global'
-            Category   = 'Security'
-            Path       = 'OU=Groups,OU=UserAccessPlane,DC=bolton,DC=corp'
-            Ensure     = 'Present'
-            DependsOn  = '[ADOrganizationalUnit]OU_UserAccessPlane_Groups'
+                GroupName  = 'GG-HR-Staff'
+                GroupScope = 'Global'
+                Category   = 'Security'
+                Path       = 'OU=Groups,OU=UserAccessPlane,DC=bolton,DC=corp'
+                Ensure     = 'Present'
+                DependsOn  = '[ADOrganizationalUnit]OU_UserAccessPlane_Groups'
             }
 
             ADGroup 'GG_Finance_Staff' {
-            GroupName  = 'GG-Finance-Staff'
-            GroupScope = 'Global'
-            Category   = 'Security'
-            Path       = 'OU=Groups,OU=UserAccessPlane,DC=bolton,DC=corp'
-            Ensure     = 'Present'
-            DependsOn  = '[ADOrganizationalUnit]OU_UserAccessPlane_Groups'
+                GroupName  = 'GG-Finance-Staff'
+                GroupScope = 'Global'
+                Category   = 'Security'
+                Path       = 'OU=Groups,OU=UserAccessPlane,DC=bolton,DC=corp'
+                Ensure     = 'Present'
+                DependsOn  = '[ADOrganizationalUnit]OU_UserAccessPlane_Groups'
             }
 
             ADGroup 'GG_IT_Admins' {
-            GroupName  = 'GG-IT-Admins'
-            GroupScope = 'Global'
-            Category   = 'Security'
-            Path       = 'OU=Groups,OU=ManagementPlane,DC=bolton,DC=corp'
-            Ensure     = 'Present'
-            DependsOn  = '[ADOrganizationalUnit]OU_ManagementPlane_Groups'
-        }
+                GroupName  = 'GG-IT-Admins'
+                GroupScope = 'Global'
+                Category   = 'Security'
+                Path       = 'OU=Groups,OU=ManagementPlane,DC=bolton,DC=corp'
+                Ensure     = 'Present'
+                DependsOn  = '[ADOrganizationalUnit]OU_ManagementPlane_Groups'
+            }
 
             ADGroup 'GG_Server_Admins' {
-            GroupName  = 'GG-Server-Admins'
-            GroupScope = 'Global'
-            Category   = 'Security'
-            Path       = 'OU=Groups,OU=ManagementPlane,DC=bolton,DC=corp'
-            Ensure     = 'Present'
-            DependsOn  = '[ADOrganizationalUnit]OU_ManagementPlane_Groups'
-        }
+                GroupName  = 'GG-Server-Admins'
+                GroupScope = 'Global'
+                Category   = 'Security'
+                Path       = 'OU=Groups,OU=ManagementPlane,DC=bolton,DC=corp'
+                Ensure     = 'Present'
+                DependsOn  = '[ADOrganizationalUnit]OU_ManagementPlane_Groups'
+            }
+
+            # =========================
+            # Tier 2 Users (UserAccessPlane)
+            # =========================
+
+            ADUser 'User_Adam_Khan' {
+                DomainName  = $Node.DomainName
+                UserName    = 'adam.khan'
+                GivenName   = 'Adam'
+                Surname     = 'Khan'
+                DisplayName = 'Adam Khan'
+                Path        = 'OU=Users,OU=UserAccessPlane,DC=bolton,DC=corp'
+                Enabled     = $true
+                Ensure      = 'Present'
+                Password    = $DomainAdminCredential
+            }
+
+            ADUser 'User_Katy_Smith' {
+                DomainName  = $Node.DomainName
+                UserName    = 'katy.smith'
+                GivenName   = 'Katy'
+                Surname     = 'Smith'
+                DisplayName = 'Katy Smith'
+                Path        = 'OU=Users,OU=UserAccessPlane,DC=bolton,DC=corp'
+                Enabled     = $true
+                Ensure      = 'Present'
+                Password    = $DomainAdminCredential
+            }
+
+            # =========================
+            # Tier 1 Users (ManagementPlane)
+            # =========================
+
+            ADUser 'User_Ismail_Admin' {
+                DomainName  = $Node.DomainName
+                UserName    = 'ismail.admin'
+                GivenName   = 'Ismail'
+                Surname     = 'Admin'
+                DisplayName = 'Ismail Admin'
+                Path        = 'OU=AdminUsers,OU=ManagementPlane,DC=bolton,DC=corp'
+                Enabled     = $true
+                Ensure      = 'Present'
+                Password    = $DomainAdminCredential
+            }
+
+            ADUser 'User_Paul_Evans' {
+                DomainName  = $Node.DomainName
+                UserName    = 'paul.evans'
+                GivenName   = 'Paul'
+                Surname     = 'Evans'
+                DisplayName = 'Paul Evans'
+                Path        = 'OU=AdminUsers,OU=ManagementPlane,DC=bolton,DC=corp'
+                Enabled     = $true
+                Ensure      = 'Present'
+                Password    = $DomainAdminCredential
+            }
+
+            # =========================
+            # RBAC Membership
+            # =========================
+
+            ADGroupMember 'Adam_HR_Membership' {
+                GroupName        = 'GG-HR-Staff'
+                MembersToInclude = @('adam.khan')
+                Ensure           = 'Present'
+                DependsOn        = '[ADUser]User_Adam_Khan','[ADGroup]GG_HR_Staff'
+            }
+
+            ADGroupMember 'Katy_Finance_Membership' {
+                GroupName        = 'GG-Finance-Staff'
+                MembersToInclude = @('katy.smith')
+                Ensure           = 'Present'
+                DependsOn        = '[ADUser]User_Katy_Smith','[ADGroup]GG_Finance_Staff'
+            }
+
+            ADGroupMember 'Ismail_ServerAdmin_Membership' {
+                GroupName        = 'GG-Server-Admins'
+                MembersToInclude = @('ismail.admin')
+                Ensure           = 'Present'
+                DependsOn        = '[ADUser]User_Ismail_Admin','[ADGroup]GG_Server_Admins'
+            }
+
+            ADGroupMember 'Paul_Helpdesk_Membership' {
+                GroupName        = 'GG-IT-Admins'
+                MembersToInclude = @('paul.evans')
+                Ensure           = 'Present'
+                DependsOn        = '[ADUser]User_Paul_Evans','[ADGroup]GG_IT_Admins'
+            }
+
         }
     }
 }
