64df17385a6531536df7896352074bf86b46c34c
ismailbutthaleem <ib2crt@bolton.ac.uk>
2026-02-23 04:38:36 +0000

    disabled passwords enforcement to debug, complier will keep complaining about password complexity

--- DSC/Configurations/StudentConfig.ps1
+++ DSC/Configurations/StudentConfig.ps1
@@ -204,28 +204,28 @@ Configuration StudentBaseline {
                 DomainName  = $Node.DomainName
                 UserName    = 'adam.khan'
                 Path        = "OU=Users,OU=UserAccessPlane,$($Node.DomainDN)"
-                Ensure      = 'False'
+                Ensure      = 'Absent'
             }
 
             ADUser 'User_Katy_Smith' {
                 DomainName  = $Node.DomainName
                 UserName    = 'katy.smith'
                 Path        = "OU=Users,OU=UserAccessPlane,$($Node.DomainDN)"
-                Ensure      = 'False'
+                Ensure      = 'Absent'
             }
 
             ADUser 'User_Ismail_Admin' {
                 DomainName  = $Node.DomainName
                 UserName    = 'ismail.admin'
                 Path        = "OU=AdminUsers,OU=ManagementPlane,$($Node.DomainDN)"
-                Ensure      = 'False'
+                Ensure      = 'Absent'
             }
 
             ADUser 'User_Paul_Evans' {
                 DomainName  = $Node.DomainName
                 UserName    = 'paul.evans'
                 Path        = "OU=AdminUsers,OU=ManagementPlane,$($Node.DomainDN)"
-                Ensure      = 'False'
+                Ensure      = 'Absent'
             }
 
         }
